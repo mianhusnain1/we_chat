e6ab7adda746e4886f78f40a0cec85f1e28b5235
mianhusnain1 <mianhusnain550@gmail.com>
2024-08-10 10:13:31 +0500

    UI Changes

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -272,7 +272,7 @@ class _SignupState extends State<Signup> {
                       Dialogs.errorDialogs(
                         context,
                         "Error",
-                        "Pleas internet connection.",
+                        "Please check your internet connection.",
                         () => Navigator.of(context).pop(),
                       );
                     } else {
